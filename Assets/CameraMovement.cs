f5df7c4cfc68d965a03d6e8bd12bca26d43a72d4
Matthew Whiteley <mw0829@my.bristol.ac.uk>
2014-08-25 22:34:21 +0100

    Added basic player X,Y movement and rotation. Locked camera to chase player position with offset.

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public Transform playerTransform;
+	private Vector3 offsetPosition = new Vector3(0, 6, -3);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = playerTransform.position + offsetPosition;
+	}
+}
---
+++ Assets/LockRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockRotation : MonoBehaviour {
+
+	private Quaternion lockValue;
+	private Vector3 lockPosition;
+
+	// Use this for initialization
+	void Start () {
+		lockValue = transform.rotation;
+		lockPosition = transform.localPosition;
+	}
+	
+	void LateUpdate () {
+		transform.rotation = lockValue;
+		transform.localPosition = lockPosition;
+	}
+}
