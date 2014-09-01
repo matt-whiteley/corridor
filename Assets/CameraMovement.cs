0399d8e3c834e1a1bd7ce74a2dbe3c4aba193527
Matthew Whiteley <mw0829@my.bristol.ac.uk>
2014-09-01 21:27:19 +0100

    adjusted camera pos, started enemy

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
 
 	public Transform playerTransform;
-	private Vector3 offsetPosition = new Vector3(0, 6, -3);
+	public Vector3 offsetPosition = new Vector3(0, 6, -3);
 	// Use this for initialization
 	void Start () {
 	
---
+++ Assets/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	int health;
+	int criticalPercentage;
+
+	void update(){
+		if (criticalPercentage > 0){
+			criticalPercentage -= Mathf.CeilToInt((criticalPercentage/100f)* 10f);
+		}
+	}
+}
